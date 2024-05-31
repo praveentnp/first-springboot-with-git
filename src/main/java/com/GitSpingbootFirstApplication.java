9c07b019a6561056baa4bb4dd2e3a7e428d4c064
praveentnp <praveenkumar.css@gmail.com>
2024-06-01 09:30:01 +1000

    First commit - files added

---
+++ src/main/java/com/GitSpingbootFirstApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpingbootFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpingbootFirstApplication.class, args);
+	}
+
+}
